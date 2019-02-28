39f3db41c1955b92471b0e79525e6db2ec7a15ce
SeanKim <sean.kim84@gmail.com>
2019-02-28 12:24:33 +0700

    Adding the React Hook

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import AppHooks from './AppHooks';
 import * as serviceWorker from './serviceWorker';
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppHooks />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
